504923cb147b8b053e0e40df0fa129a887b085f8
Yohan <35965925+YohanMA@users.noreply.github.com>
2018-04-05 12:16:48 +0200

    Add files via upload

--- src/java/sql/trigger_before_insert_personne.sql
+++ src/java/sql/trigger_before_insert_personne.sql
@@ -1,11 +1,27 @@
 DELIMITER $$
+
 DROP TRIGGER IF EXISTS before_insert_personne $$
 CREATE TRIGGER before_insert_personne 
 BEFORE INSERT ON personne 
 FOR EACH ROW
 BEGIN
-	SET NEW.nom = UPPER(NEW.nom);
-	SET NEW.prenom = CONCAT(UPPER(LEFT(NEW.prenom, 1)), LOWER(SUBSTR(NEW.prenom, 2)));
-	SET NEW.tel = TRIM(NEW.tel);
+	DECLARE i INT DEFAULT 1;
+	DECLARE tel VARCHAR(10) DEFAULT '';
+
+	SET NEW.nom := UPPER(NEW.nom);
+	SET NEW.prenom := CONCAT(UPPER(LEFT(NEW.prenom, 1)), LOWER(SUBSTR(NEW.prenom, 2)));
+
+	WHILE i < LENGTH(NEW.tel)+1 DO
+		BEGIN
+			IF SUBSTR(NEW.tel, i, 1) != ' ' THEN
+				SET tel := TRIM(CONCAT(tel, SUBSTR(NEW.tel, i, 1)));
+			END IF;
+			SET i := i+1;
+		END;
+	END WHILE;
+
+	SET NEW.tel := tel;
+
 END $$
+
 DELIMITER ;
